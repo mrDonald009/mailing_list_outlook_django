db036d5d82fb29064219bc4d74ad512015e3c067
mrDon <mrdonald009@gmail.com>
2022-10-01 00:12:18 +0300

    outlook_django

---
+++ virtual/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\alfa_capital\mailing_list_outlook_django\virtual\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
